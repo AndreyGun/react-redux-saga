222c0ac5d26431d5071cc7ca088d98c0a55fc048
Andrey <andreygunkin90@gmail.com>
2020-04-02 16:57:22 +0300

    fun create posts commit (REDUX - ON)

---
+++ src/components/Post.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+export default ({post}) => {
+    return(
+        <div className="card">
+            <div className="card-body">
+                <h2 className="card-title">{post.title}</h2>
+            </div>
+        </div>
+    )
+}
